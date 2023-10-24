e7789e8d8ef01a0661f4b0304b3858b9522298cb
KRonae <112303878+KR-onae@users.noreply.github.com>
2023-10-24 21:38:34 +0900

    Create RegexSplit.java

---
+++ RegexSplit.java
@@ -0,0 +1,22 @@
+package com.kronae.encrypt.enb64.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// IntelliJ
+import org.jetbrains.annotations.NotNull;
+
+public class RegexSplit {
+    public static String @NotNull [] split(String input, String regex) {
+        List<String> matches = new ArrayList<>();
+        Matcher m = Pattern.compile(regex)
+                .matcher(input);
+
+        while (m.find())
+            matches.add(m.group());
+
+        return matches.toArray(new String[0]);
+    }
+}
