f513b6c084c5b34cbfe1f8cf1ee588865db0fe3d
KRonae <112303878+KR-onae@users.noreply.github.com>
2023-10-24 21:37:12 +0900

    Create ENB64.java

---
+++ ENB64.java
@@ -0,0 +1,54 @@
+package com.kronae.encrypt.enb64;
+
+import com.kronae.encrypt.enb64.exception.DataTooLotException;
+import com.kronae.encrypt.enb64.util.RegexSplit;
+
+public class ENB64 extends Encrypter { // Encrypt aNd Base64 encrypting system
+    private final char[] key;
+
+    public ENB64(@NotNull String key) {
+        this.key = key.toCharArray();
+    }
+    public @NotNull String encrypt(@NotNull String decrypted) {
+        char[] chars = decrypted.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            for (char c : key) {
+                chars[i] = (char) (chars[i] + c);
+            }
+        }
+
+        String encrypted = new String(chars);
+
+        return B64.encode(encrypted) + "$";
+    }
+    public String[] decryptAll(@NotNull String encrypted) {
+        if(!encrypted.endsWith("$"))
+            return null;
+
+        String[] arr = RegexSplit.split(encrypted, B64.regex);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = decrypt(arr[i]);
+
+        return arr;
+    }
+    public @NotNull String decrypt(@NotNull String encrypted) {
+        if(count(encrypted) > 1)
+            throw new DataTooLotException("Encrypted data is too lot! Please use ENB64S#decryptAll(String) or user ENB64S.count(String) to get count");
+
+        String decoded = B64.decodeOne(encrypted);
+
+        char[] chars = decoded.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            for (char c : key) {
+                chars[i] = (char) (chars[i] - c);
+            }
+        }
+
+        return new String(chars);
+    }
+
+    @Contract(pure = true)
+    public static int count(@NotNull String encrypted) {
+        return encrypted.split("\\$").length;
+    }
+}
